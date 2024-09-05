c8c4304ad55da225b662fb65f9c5c663f00f7ccf
homei-leung <49667943+homei-leung@users.noreply.github.com>
2024-09-05 19:21:28 -0400

    add playRound function

--- script.js
+++ script.js
@@ -14,20 +14,48 @@ function getComputerChoice() {
     }
 }
 
-// log what getComputerChoice does in the console
-console.log(getComputerChoice());
-
 // Create a function named getHumanChoice
 function getHumanChoice() {
     let choice;
     do {
         choice = prompt("Rock, paper, scissors, shoot! Choose your action:");
     }
-    // while the input is not "rock", "paper", or "scissors", continue prompt
+    // While the input is not "rock", "paper", or "scissors", continue prompt
     while (choice !== "rock" && choice !== "paper" && choice !== "scissors");
     return choice;
 }
 
+// Initialize player score variables
+let humanScore = 0;
+let computerScore = 0;
 
-// log what getHumanChoice does in the console
-console.log(getHumanChoice());
+// Write a function to play a single round
+function playRound(humanChoice, computerChoice) {
+    // Make humanChoice case-insensitive
+    let anyCase = humanChoice.toLowerCase();
+    // Paper beats rock
+    // Rock beats scissors
+    // Scissors beats Paper
+    // Draw if equal
+    if (anyCase === "rock" && computerChoice === "paper") {
+        computerScore++;
+        return "You lose! Paper beats rock.";
+    }else if (anyCase === "scissors" && computerChoice === "rock") {
+        computerScore++;
+        return "You lose! Rock beats scissors.";
+    }else if (anyCase === "paper" && computerChoice === "scissors") {
+        computerScore++;
+        return "You lose! Scissors beats paper.";
+    }else if (anyCase === computerChoice) {
+        return "Draw!";
+    } else {
+        humanScore++;
+        return "You win!";
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+// Log playRound function
+console.log(playRound(humanSelection, computerSelection));
